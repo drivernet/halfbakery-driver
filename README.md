d9196720b3e05826805a50afbb303c3fdba66183
Mindey <mindey@qq.com>
2018-12-22 15:57:34 +0000

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Halfbakery Driver
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='halfbakery',
+    version='0.0.1',
+    description='Halfbakery Communal Database controller.',
+    url='https://gitlab.com/wefindx/halfbakery',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='ASK FOR PERMISSIONS',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=['metadrive'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    zip_safe=False
+)
