577d18c43fb14f006c63815ea9addd11a81c509f
Mindey <mindey@qq.com>
2019-06-17 15:09:44 +0000

    extend _login with interactive param

--- halfbakery_driver/__init__.py
+++ halfbakery_driver/__init__.py
@@ -1,6 +1,8 @@
 __site_url__ = 'http://www.halfbakery.com'
 __base_url__ = 'https://www.halfbakery.com/lr/'
 
+import bs4
+import getpass
 from tqdm import tqdm
 from metadrive._requests import get_drive
 
@@ -11,11 +13,21 @@ def _login(
         drive=None,
         refresh=False,
         recreate_profile=False,
-        profile='halfbakery:default',
+        profile='halfbakery-driver:default',
         proxies='default',
+        interactive=False,
     ):
 
+    if interactive:
+        if username is None:
+            username = input('Halfbakery username: ') or None
+        if password is None:
+            password = getpass.getpass('Halfbakery password: ') or None
+
     if drive is None:
+
+        if username is not None:
+            profile = 'halfbakery-driver:{}'.format(username)
 
         drive = get_drive(
             profile=profile,
@@ -25,6 +37,7 @@ def _login(
     drive.get(__base_url__)
 
     if drive.response.ok:
+
         drive.get(__base_url__,
              params={
                  'username': username,
@@ -32,6 +45,23 @@ def _login(
                  'login': 'login'})
 
         if drive.response.ok:
+
+            soup = bs4.BeautifulSoup(drive.response.content, 'html.parser')
+            if soup.find('a', text='logout') is None:
+
+                if username is None:
+
+                    drive.get(__site_url__)
+                    soup = bs4.BeautifulSoup(drive.response.content, 'html.parser')
+                    edit = soup.find('a', text='edit')
+
+                    if edit is not None:
+                        username = bs4.BeautifulSoup(repr(edit.parent).split(repr(edit))[0], 'html.parser').text.strip()
+                    else:
+                        username = None
+
+            print('State: logged in as {}.'.format(username or '<unknown>'))
+
             return drive
         else:
             raise Exception(
@@ -43,7 +73,6 @@ def _login(
                 drive.response.status_code))
 
     return drive
-
 
 
 def _harvest(drive=None, period=60):
